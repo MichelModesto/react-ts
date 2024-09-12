2ae10e6e7ec36d1232f62b50472d4263e4d650a7
MichelModesto <michel.mx10@gmail.com>
2024-09-12 15:15:45 -0300

    final react-js

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,6 @@ import styles from './Header.module.css'
 
 import igniteLogo from '../assets/ignite-logo.svg'
 
-
 export function Header() {
     return (
         <header className={styles.header}>
